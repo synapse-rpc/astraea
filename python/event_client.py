79a39750773069783cf498393ccd94b8dd91ffe6
xRain <xrain@simcu.com>
2016-07-10 15:21:06 +0800

    fix event client

--- python/event_client.py
+++ python/event_client.py
@@ -5,18 +5,20 @@ from .base import Base
 class EventClient(Base):
     @classmethod
     def send_event(self,action,params):
-        print("Send A RPC REQUEST")
-        data = {
-            "from": self.app_name,
-            "to" : "event",
-            "action": action,
-            "params": params
-        }
-        self.mqch.basic_publish(exchange=self.sys_name,
-                                routing_key="event."+self.app_name+"."+action,
-                                   properties=pika.BasicProperties(
-                                         correlation_id = str(uuid.uuid4()),
-                                         ),
-                                   body=json.dumps(data))
-        if self.debug:
-                self.log("[Synapse Debug] Publish A Event: %s.%s %s" % (self.app_name,action,data))
+        if self.disable_event_client:
+            self.log("[Synapse Error] Event Send Not Success: DisableEventClient set true")
+        else:
+            data = {
+                "from": self.app_name,
+                "to" : "event",
+                "action": action,
+                "params": params
+            }
+            self.mqch.basic_publish(exchange=self.sys_name,
+                                    routing_key="event."+self.app_name+"."+action,
+                                    properties=pika.BasicProperties(
+                                            correlation_id = str(uuid.uuid4()),
+                                            ),
+                                    body=json.dumps(data))
+            if self.debug:
+                    self.log("[Synapse Debug] Publish Event: %s.%s %s" % (self.app_name,action,data))
